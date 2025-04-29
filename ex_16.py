ce3c2f52684bb82e6413974be51716344f53329e
rjaniak <rajkonkret660@gmail.com>
2025-04-29 11:56:31 +0200

    [2 11] update ex16

---
+++ ex_16.py
@@ -0,0 +1,22 @@
+import pandas as pd
+
+height_data = [
+    {"Name": "Aditya", "Height": 179},
+    {"Name": "Sameer", "Height": 181},
+    {"Name": "Darwin", "Height": 170},
+    {"Name": "Aditya", "Height": 167},
+]
+
+weight_data = [
+    {"Name": "Aditya", "Weight": 76},
+    {"Name": "Sameer", "Weight": 68},
+    {"Name": "Darwin", "Weight": 69},
+    {"Name": "Aditya", "Weight": 73},
+]
+
+marks_data = [
+    {"Name": "Aditya", "Marks": 179},
+    {"Name": "Sameer", "Marks": 181},
+    {"Name": "Darwin", "Marks": 170},
+    {"Name": "Aditya", "Marks": 167},
+]
